2024d2d83cf2629b66a010ecf89bf85f3e43bae8
Peter Persson <peter.persson@bilvision.se>
2020-01-23 10:31:15 +0100

    WIP

--- src/components/prospect/subcomponents/prospect_sub_component2.js
+++ src/components/prospect/subcomponents/prospect_sub_component2.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const ProspectSubComponent2 = () => {
     return (
         <div>
-            Prospektera sub-komponent 2. Har tillgång till state?
+            Prospektera sub-komponent 2.
         </div>
     );
 };
