96b4ed9fddf921defe5ccd237b1009bb3116e503
Peter Persson <peter.persson@bilvision.se>
2020-03-24 13:09:47 +0100

    Color changes

--- src/components/common/footer/breadcrumbs/breadcrumbs.js
+++ src/components/common/footer/breadcrumbs/breadcrumbs.js
@@ -6,6 +6,7 @@ import {connect} from 'react-redux';
 const routes = {
     affar: 'Affär',
     aktivitet: 'Aktivitet',
+    analysera: 'Analysera',
     bearbeta: 'Bearbeta',
     foretag: 'Företag',
     hem: 'Hem',
